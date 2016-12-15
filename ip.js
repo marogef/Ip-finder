de1d25a92545effcd32c6f957a34b92400f8ab2c
Faras Maroge <Goldencrane33@yahoo.com>
2016-12-14 20:49:21 -0800

    errors fix
    added the correct functionality, properties for the API in order to
    work properly.

--- ip.js
+++ ip.js
@@ -1,23 +1,21 @@
 $(document).ready(function () {
-            $("ip-search").submit(function (e) {
-                e.preventDefault();
-                var searchValue = $("search-input").val();
-                display(searchValue);
-            });
+    var searchValue = "";
 
-            function display(searchValue) {
-                $.getJSON('http://ipinfo.io', function (data) {
-                    console.log(data)
-                }), {
-                    "ip": "203.205.28.14",
-                    "hostname": "static.cmcti.vn",
-                    "city": "Ho Chi Minh City",
-                    "region": "Ho Chi Minh City",
-                    "country": "VN",
-                    "loc": "10.8142,106.6438",
-                    "org": "AS45903 CMC Telecom Infrastructure Company"
-                });
+    $.getJSON('http://ipinfo.io/', function (data) {
+        searchValue = data.ip;
+    })
 
-            $.get("http://ipinfo.io", function (response) {
-                console.log(response.ip, response.country);
-            }, "jsonp");
+    $("#ip-search").submit(function (e) {
+        e.preventDefault();
+        searchValue = $("#search-input").val();
+        display(searchValue);
+    });
+
+    function display(searchValue) {
+
+        $.getJSON('http://ipinfo.io/' + searchValue + '/json', function (data) {
+            console.log(data);
+        })
+    }
+
+});
