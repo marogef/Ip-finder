e3f7bcdc055e9a5552ab1ceed816fdb6ab435f1f
Faras Maroge <Goldencrane33@yahoo.com>
2016-12-27 19:59:11 -0500

    table, table contents
    added table, table content, color properties.

--- ip.js
+++ ip.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
     //hide the results table
     $('.table').hide();
-    //    $('.map').hide();
+
+
     //declare the search value inside the input text
     var searchValue = "";
 
-
-
-    //http://ipinfo.io/json for your own IP
+    //use http://ipinfo.io/json for your own IP
     $.getJSON('http://ipinfo.io/', function (data) {
         //set the initial search value to the user ip
         searchValue = data.ip;
@@ -15,30 +14,30 @@ $(document).ready(function () {
     });
 
     //when the user clicks the search button show the results
+
     $("#ip-search").submit(function (e) {
         //show the results to the user
         $('.table').show();
-        //        $('.map').show();
+
         //default action will not be triggered when method is called
         e.preventDefault();
         //get value from search box
         searchValue = $("#search-input").val();
-        //validate
+        //validate the value
         if (searchValue.length === 0) {
             alert('You have to add something!!!');
         } else {
             //pass value to the searchIp function to do the ip lookup and integrate that into the script
             searchIp(searchValue);
-            //            initialize(searchValue);
         }
     });
     //function for doing the IP lookup using the user's input value
     function searchIp(searchValue) {
-
         //Use the searchValue to get the details on the user input IP
+
         $.getJSON('http://ipinfo.io/' + searchValue + '/json', function (data) {
             //add values to the table dynamically
-            //declare a variable tr and populate it
+            //declare a variable and populate it
             var htmlOutput = "";
             htmlOutput += "<li>" + "IP address: " + data.ip + "</li>";
             htmlOutput += "<li>" + "Hostname: " + data.hostname + "</li>";
@@ -49,5 +48,6 @@ $(document).ready(function () {
             htmlOutput += "<li>" + "organization: " + data.org + "</li>";
             $('.table').append(htmlOutput);
         });
+
     }
 });
